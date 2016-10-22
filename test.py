b03ecf205c1fb05380d46193406dec338c1eb49a
xiaomin0322 <xiaomin0322@sian.com>
2016-10-23 00:01:43 +0800

    update

--- test.py
+++ test.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 
 from wxbot import *
+from __main__ import time
 
 
 class MyWXBot(WXBot):
@@ -12,33 +13,45 @@ class MyWXBot(WXBot):
             #self.send_file_msg_by_uid("img/1.png", msg['user']['id'])
 
     def schedule(self):
-        useridKedou = self.get_user_id('小蝌蚪');
+    
+            useridKedou = self.get_user_id('小蝌蚪')
+                
+            userid = self.get_user_id('毛哥')
+                
+            print  'userid ' + userid
+                
+               
+            rd = random.randint(0, 20)
+                
+            print 'time is '+ time.strftime('%Y-%m-%d %X', time.localtime())
+                
+            print  'random >>>>>>>'+bytes(rd)
+                 
+            if rd > 18:
+                print '发送消息了'
+                time.sleep(2) 
+                self.send_msg_by_uid('毛哥，你屎坐了吗 ？ 呼叫毛哥！！！！毛哥是否尚在????????',userid)
+                time.sleep(2)
+                self.send_img_msg_by_uid("img/login_on_ubuntu.png", userid)
+               
+                
+                #self.send_msg(u'小蝌蚪', u'测试')
+          
+                #self.send_msg('毛哥', 'mao哥你好')
+          
+               # self.send_msg('怀化人在上海微信群', '111')
+          
+                     
+               # self.send_img_msg_by_uid("img/login_on_ubuntu.png", userid)
+               
+               
+               # time.sleep(4)
+               
+               # self.send_img_msg_by_uid("e:/test.png", useridKedou)
+               
+                
+               # time.sleep(20)
         
-        userid = self.get_user_id('毛哥');
-        
-        print  'userid ' + userid
-        
-  
-        
-        #self.send_msg(u'小蝌蚪', u'测试')
-  
-        #self.send_msg('毛哥', 'mao哥你好')
-  
-       # self.send_msg('怀化人在上海微信群', '111')
-  
-  #@@08807a0121684fc46ceb038dfefeee230e9a0f688ada84df8800a2c3cbbd7491
-        
-        
-        self.send_msg_by_uid('呼叫毛哥！！！！毛哥是否尚在????????',userid)
-        
-        #self.send_img_msg_by_uid("img/1.png", userid)
-        self.send_img_msg_by_uid("e:/test.png", userid)
-       
-        self.send_img_msg_by_uid("e:/test.png", useridKedou)
-       
-        
-        time.sleep(20)
-
 
 
 def main():
@@ -46,7 +59,8 @@ def main():
     bot.DEBUG = True
     bot.conf['qr'] = 'png'
     bot.run()
-
+    print 'time is '+ time.strftime('%Y-%m-%d %X', time.localtime())
+       
 
 if __name__ == '__main__':
     main()
